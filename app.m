3e39e9d2945a6908bdbdb1980dababcb86122e61
Jean-Baptiste Lanfrey <jlanfrey@mathworks.com>
2020-06-30 09:17:01 +1000

    simplify app

--- app.m
+++ app.m
@@ -1,33 +1,26 @@
-function app()
-  ds = fileDatastore("data", "ReadFcn",@load);
-  fig = uifigure;
-  gridLayout = uigridlayout(fig, [2,2]);
-  gridLayout.RowHeight = {80, "1x"};
-  gridLayout.ColumnWidth = {150, "1x"};
-  
-  listOfFiles = ds.Files;
-  listOfFiles = extractBetween(listOfFiles, "activity-", ".mat");
-  fileList = uilistbox(gridLayout, 'Items', listOfFiles, 'ValueChangedFcn', @updateEditField);
-  fileList.Layout.Row = [1 2];
-  fileList.Layout.Column = 1;
-  
-  summaryPanel = uipanel(gridLayout);
-  summaryPanel.Layout.Row = 1;
-  summaryPanel.Layout.Column = 2;
-  
-  geoaxesPanel = uipanel(gridLayout);
-  geoaxesPanel.Layout.Row = 2;
-  geoaxesPanel.Layout.Column = 2;
-  
-  % Model
-  act = activity;
-  % Summary Viewer
-  summaryViewer = activitySummaryViewer(summaryPanel, act);
-  % Map Viewer
-  mapViewer = activityMapViewer(geoaxesPanel, act);
-    
-  % ValueChangedFcn callback
-  function updateEditField(src,~)
-    act.updateActivity("activity-" + src.Value + ".mat");
-  end
-end
+fig = uifigure;
+gridLayout = uigridlayout(fig, [2,2]);
+gridLayout.RowHeight = {80, "1x"};
+gridLayout.ColumnWidth = {150, "1x"};
+
+controllerPanel = uipanel(gridLayout);
+controllerPanel.Layout.Row = [1 2];
+controllerPanel.Layout.Column = 1;
+
+summaryPanel = uipanel(gridLayout);
+summaryPanel.Layout.Row = 1;
+summaryPanel.Layout.Column = 2;
+
+geoaxesPanel = uipanel(gridLayout);
+geoaxesPanel.Layout.Row = 2;
+geoaxesPanel.Layout.Column = 2;
+
+% Model
+act = activity;
+% Controller
+Controller = activityController(controllerPanel, act);
+% Summary Viewer
+summaryViewer = activitySummaryViewer(summaryPanel, act);
+% Map Viewer
+mapViewer = activityMapViewer(geoaxesPanel, act);
+
